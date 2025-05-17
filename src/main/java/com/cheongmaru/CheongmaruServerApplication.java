0b2312faf1f3257dcb00d3cce2edcebe10c18933
zoo0o <qmdlxlr@gmail.com>
2025-05-17 22:00:04 +0900

    init

---
+++ src/main/java/com/cheongmaru/CheongmaruServerApplication.java
@@ -0,0 +1,13 @@
+package com.cheongmaru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheongmaruServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheongmaruServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cheongmaru/CheongmaruServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheongmaru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheongmaruServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
